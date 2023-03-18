2b16a860f2110bd17cbca1e85f77cbde8b57af9b
Sachin Maurya <100995198+SachinMaurya01@users.noreply.github.com>
2023-03-18 09:27:10 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -11,7 +11,7 @@ const Feed = () => {
   useEffect(() => {
     setVideos(null);
 
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    fetchFromAPI(`searchpart=snippet&q=${selectedCategory}`)
       .then((data) => setVideos(data.items))
     }, [selectedCategory]);
 
